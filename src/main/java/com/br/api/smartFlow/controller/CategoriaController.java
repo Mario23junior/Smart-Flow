e3333a37f037e19409f4d10509d5d8e73faa4a0d
Mario23junior <mariojunior3251@gmail.com>
2022-03-19 21:10:38 -0300

    Criando controller de categorias

---
+++ src/main/java/com/br/api/smartFlow/controller/CategoriaController.java
@@ -0,0 +1,10 @@
+package com.br.api.smartFlow.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/categorias")
+public class CategoriaController {
+
+}
