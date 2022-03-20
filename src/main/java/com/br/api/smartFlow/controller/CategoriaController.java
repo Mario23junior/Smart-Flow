f8736d6ab7e6788dc5a975554ab4bc22ad25ee5c
Mario23junior <mariojunior3251@gmail.com>
2022-03-19 21:45:44 -0300

    Testando endpoints de listagem

--- src/main/java/com/br/api/smartFlow/controller/CategoriaController.java
+++ src/main/java/com/br/api/smartFlow/controller/CategoriaController.java
@@ -1,10 +1,28 @@
 package com.br.api.smartFlow.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/api/v1/categorias")
-public class CategoriaController {
+import com.br.api.smartFlow.model.Categoria;
 
+@RestController
+@RequestMapping("/api/v1/categorias/")
+public class CategoriaController {
+   
+	@GetMapping
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1l,"Informatica");
+		Categoria cat2 = new Categoria(2l,"Escritorio");
+		
+		List<Categoria> lista = new ArrayList<Categoria>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
+	}
 }
